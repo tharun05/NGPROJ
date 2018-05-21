625f4a2836304beec3f2d41732347cc9365b2485
unknown <dshahala@gmail.com>
2018-05-21 12:20:13 +0530

    commit

--- src/app/workshop/workshop.module.ts
+++ src/app/workshop/workshop.module.ts
@@ -4,14 +4,13 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule,Router } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
-
-
 import { EditComponent } from '../workshop/profile/edit/edit.component';
 import { ViewComponent } from '../workshop/profile/view/view.component';
 import { UserprofileComponent } from '../workshop/users/userprofile/userprofile.component';
 import { UserslistComponent } from '../workshop/users/userslist/userslist.component';
 
 import { ProfileService } from '../workshop/profile/profile.service';
+import { MatInputModule } from '@angular/material';
 
 const routes: Routes = [
   { path: 'myProfile-edit', component: EditComponent},
