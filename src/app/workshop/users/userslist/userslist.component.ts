e3fce8cebde2e5618ab000cecba9ecebea6a0269
unknown <dshahala@gmail.com>
2018-05-22 14:16:14 +0530

    pipe

--- src/app/workshop/users/userslist/userslist.component.ts
+++ src/app/workshop/users/userslist/userslist.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import{Routes,Router} from '@angular/router'
 import { Route } from '@angular/compiler/src/core';
 import {UserService} from '../users.service'
-
+import {FormBuilder,FormGroup,FormControl, Validators} from '@angular/forms';
+import {ReverseString} from '../../pipe'
 @Component({
   selector: 'app-userslist',
   templateUrl: './userslist.component.html',
@@ -10,23 +11,28 @@ import {UserService} from '../users.service'
 })
 export class UserslistComponent implements OnInit {
 userList:any;
-userData:any;
-  constructor(private router:Router, private userService: UserService) { 
-    this.getUsers();
+userDetails:FormGroup;
+
+iFilter: any = "";
+  constructor(private router:Router,private userService:UserService,private formBuilder:FormBuilder) { 
+    this.searchUsers();
+    this.userDetails = this.formBuilder.group({
+      'name':['',[Validators.required]],
+    })
   }
 
   ngOnInit() {
   }
+  navigateToProfile(){
+    this.router.navigate(['userprofile'],{queryParams: {id:17185318726}})
 
-getUsers(){
-  this.userService.searchUsers().subscribe((data:any)=>{
-    if(data){
-      this.userData = data.message;
+  }
 
-    }
-  })
-}
-navigateToProfile(user:any){
-this.router.navigate(['userprofile'],{queryParams :{id:user._id}})
-}
+  searchUsers(){
+    this.userService.searchUsers().subscribe((data)=>{
+      console.log(data)
+      this.userList = data.message;
+    })
+  }
+
 }
--- src/app/workshop/workshop.module.ts
+++ src/app/workshop/workshop.module.ts
@@ -4,14 +4,14 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule,Router } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditComponent } from '../workshop/profile/edit/edit.component';
 import { ViewComponent } from '../workshop/profile/view/view.component';
 import { UserprofileComponent } from '../workshop/users/userprofile/userprofile.component';
 import { UserslistComponent } from '../workshop/users/userslist/userslist.component';
-
 import { ProfileService } from '../workshop/profile/profile.service';
-import { MatInputModule } from '@angular/material';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {ReverseString,FilterPipe} from './pipe'
 
 const routes: Routes = [
   { path: 'myProfile-edit', component: EditComponent},
@@ -25,14 +25,18 @@ const routes: Routes = [
     CommonModule,
     MatToolbarModule,
     MatCardModule,
+    MatInputModule,
+    FormsModule,
     RouterModule.forRoot(routes, { useHash: false }),
-    MatFormFieldModule
   ],
   declarations: [
     EditComponent,
     ViewComponent,
     UserprofileComponent,
-    UserslistComponent
+    UserslistComponent,
+    ReverseString,
+    FilterPipe
+    
   ],
   exports: [RouterModule],
   providers: [UserService, ProfileService]
